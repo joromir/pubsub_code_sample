5fd178f2205f1de890b80f47087954151f2bf170
Georgi Kostov <gnkostov@yahoo.com>
2017-05-04 11:23:39 +0300

    refactor PubSubRedis::Broker#run

--- lib/pubsub_redis/broker.rb
+++ lib/pubsub_redis/broker.rb
@@ -2,19 +2,18 @@ module PubSubRedis
   # :nodoc:
   class Broker
     attr_reader :path
-    attr_reader :server
-
     attr_accessor :clients
 
     def initialize(path = LocationTuple.new)
       @path        = path
-      @server      = TCPServer.new(path.host, path.port)
       @connections = {}
       @topics      = {}
       @clients     = []
     end
 
     def run
+      server = TCPServer.new(path.host, path.port)
+
       loop do
         Thread.start(server.accept) do |connection|
           Topic.new(self, connection).execute
