4a348fc8cf4671408a3635b0c4d8dd7f5933e186
Georgi Kostov <gnkostov@yahoo.com>
2017-05-06 12:17:46 +0300

    add missing specs for PubSubRedis::MinutesAgo module

---
+++ spec/pubsub_redis/minutes_ago_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+RSpec.describe PubSubRedis::MinutesAgo do
+  let(:dummy_class) do
+    Class.new do
+      include PubSubRedis::MinutesAgo
+    end
+  end
+
+  subject { dummy_class.new }
+
+  describe '#expired?' do
+    context 'when timestamp is in the range of 30 minutes' do
+      let(:timestamp) { Time.now.to_i }
+
+      it 'returns true' do
+        expect(subject.expired?(timestamp)).to eq true
+      end
+    end
+
+    context 'when timestamp is NOT in the range of 30 minutes' do
+      let(:timestamp) { subject.minutes_ago(50) }
+
+      it 'returns false' do
+        expect(subject.expired?(timestamp)).to eq false
+      end
+    end
+  end
+
+  describe '#minutes_ago' do
+    it 'substracts 30 minutes from the current time' do
+      sample_format = '%Y-%M-%D-%H-%M'
+      expected = (Time.now - (30 * 60)).strftime(sample_format)
+      expect(subject.minutes_ago(30).strftime(sample_format)).to eq expected
+    end
+  end
+end
