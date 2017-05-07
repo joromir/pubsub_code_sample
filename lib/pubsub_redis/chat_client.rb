801077ebd3419b42be54a757f360133535d91ebe
Georgi Kostov <gnkostov@yahoo.com>
2017-05-08 00:08:22 +0300

    create a rake task for fireing up chat

--- lib/pubsub_redis/chat_client.rb
+++ lib/pubsub_redis/chat_client.rb
@@ -5,14 +5,14 @@ module PubSubRedis
     attr_reader :publisher
 
     def initialize
-      @publisher = Publisher.new
+      @publisher = PubSubRedis::Publisher.new
     end
 
     def run
       listen_topic
 
       loop do
-        publisher.execute('topic' => 'cars', 'body' => gets.chomp)
+        publisher.execute('topic' => 'cars', 'body' => STDIN.gets.chomp)
       end
     end
 
@@ -22,7 +22,7 @@ module PubSubRedis
       Thread.new do
         PubSubRedis::Subscriber.new do |user|
           user.enroll('cars')
-          user.listen { |message| puts "[MESSAGE] : #{message}" }
+          user.listen
         end
       end
     end
