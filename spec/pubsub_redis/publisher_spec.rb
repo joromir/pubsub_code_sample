34cc65d6031429fb173483e79a54da10d67092b3
Georgi Kostov <gnkostov@yahoo.com>
2017-05-07 16:40:25 +0300

    add specs

--- spec/pubsub_redis/publisher_spec.rb
+++ spec/pubsub_redis/publisher_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 RSpec.describe PubSubRedis::Publisher do
+  describe '#path' do
+    it 'responds to path' do
+      expect(subject).to respond_to :path
+    end
+  end
 end
